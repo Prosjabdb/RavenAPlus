90f5028d53adf5fcca4b3d543f271b5620fee9c0
Prosjabdb <113817586+Prosjabdb@users.noreply.github.com>
2026-02-05 09:25:36 +0530

    Update IMoreKB.java

--- src/main/java/keystrokesmod/module/impl/combat/morekb/IMoreKB.java
+++ src/main/java/keystrokesmod/module/impl/combat/morekb/IMoreKB.java
@@ -4,6 +4,7 @@ import keystrokesmod.module.Module;
 
 public abstract class IMoreKB extends Module {
     private boolean canSprint = true;
+    private boolean sprintWasPressed = false;
 
     public IMoreKB(String name, category moduleCategory) {
         super(name, moduleCategory);
@@ -11,13 +12,28 @@ public abstract class IMoreKB extends Module {
 
     public void stopSprint() {
         canSprint = false;
+        sprintWasPressed = mc.thePlayer.isSprinting() || mc.gameSettings.keyBindSprint.isKeyDown();
     }
 
     public void reSprint() {
         canSprint = true;
+        if (sprintWasPressed && mc.thePlayer != null) {
+            mc.thePlayer.setSprinting(true);
+        }
+        sprintWasPressed = false;
     }
 
     protected boolean noSprint() {
         return !canSprint;
+    }
+
+    protected boolean shouldReSprint() {
+        return sprintWasPressed;
+    }
+
+    @Override
+    public void onDisable() {
+        canSprint = true;
+        sprintWasPressed = false;
     }
 }
